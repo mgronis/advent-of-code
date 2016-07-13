a7b78bc7ef45718ea0c2e514c3d2414ea90422f4
mgronis <m.gronis@gmail.com>
2016-07-13 12:03:47 +0200

    More tests and tied every thing together, also fixed the implementation, right rectangular prism have 6 sides not 3 =)

--- elixir_style/lib/day2/day2_1.ex
+++ elixir_style/lib/day2/day2_1.ex
@@ -23,37 +23,56 @@ defmodule Day2_1 do
   # All numbers in the elves' list are in feet. How many total square feet of
   # wrapping paper should they order?
 
+  def calculate do
+    calculate_with_file("lib/day2/input")
+  end
+
+  def calculate_with_file(filename) do
+    read_file(filename)
+    |> create_list_of_sides
+    |> Enum.map(fn(sides) -> wrapping_paper_for_one_present(sides) end)
+    |> Enum.reduce(0, fn (x, acc) -> x + acc end)
+  end
+
   def read_file(filename) do
     {:ok, data} = File.open(filename, [:read], fn(file) -> IO.read(file, :all) end)
     data
   end
 
   def create_list_of_sides(string_input) do
-    String.split(string_input, "\n", trim: true) |> Enum.map(fn(str) -> String.split(str, "x") end)
+    String.split(string_input, "\n", trim: true)
+    |> Enum.map(
+      fn(str) -> String.split(str, "x")
+      |> Enum.map(fn str -> String.to_integer(str)  end)
+    end)
+  end
+
+  def wrapping_paper_for_one_present([a, b, c]) do
+    wrapping_paper_for_one_present(a, b, c)
   end
 
   def wrapping_paper_for_one_present(a, b, c) when a <= b and b <= c do
-    a*b + a*c + c*b + extra_wrapping_paper(a, b)
+    2 * (a*b + a*c + c*b) + extra_wrapping_paper(a, b)
   end
 
   def wrapping_paper_for_one_present(a, b, c) when a <= c and b <= c do
-    a*b + a*c + c*b + extra_wrapping_paper(a, b)
+    2 * (a*b + a*c + c*b) + extra_wrapping_paper(a, b)
   end
 
   def wrapping_paper_for_one_present(a, b, c) when b <= c and c <= a do
-    a*b + a*c + c*b + extra_wrapping_paper(b, c)
+    2 * (a*b + a*c + c*b) + extra_wrapping_paper(b, c)
   end
 
   def wrapping_paper_for_one_present(a, b, c) when a <= c and c <= b do
-    a*b + a*c + c*b + extra_wrapping_paper(a, c)
+    2 * (a*b + a*c + c*b) + extra_wrapping_paper(a, c)
   end
 
   def wrapping_paper_for_one_present(a, b, c) when c <= a and b <= a do
-    a*b + a*c + c*b + extra_wrapping_paper(b, c)
+    2 * (a*b + a*c + c*b) + extra_wrapping_paper(b, c)
   end
 
   def wrapping_paper_for_one_present(a, b, c) when a <= b and c <= a do
-    a*b + a*c + c*b + extra_wrapping_paper(a, c)
+    2 * (a*b + a*c + c*b) + extra_wrapping_paper(a, c)
   end
 
   def extra_wrapping_paper(x, y) do
--- elixir_style/test/day2/day2_1_test.exs
+++ elixir_style/test/day2/day2_1_test.exs
@@ -3,31 +3,31 @@ defmodule Day2_1_Test do
   import Day2_1
 
   test "calculate wrapping paper for one cube shaped present" do
-    assert wrapping_paper_for_one_present(4, 4, 4) == 64
+    assert wrapping_paper_for_one_present(4, 4, 4) == 112
   end
 
   test "calculate wrapping paper for one non cube shaped present 1" do
-    assert wrapping_paper_for_one_present(2, 3, 4) == 32
+    assert wrapping_paper_for_one_present(2, 3, 4) == 58
   end
 
   test "calculate wrapping paper for one non cube shaped present 2" do
-    assert wrapping_paper_for_one_present(2, 4, 3) == 32
+    assert wrapping_paper_for_one_present(2, 4, 3) == 58
   end
 
   test "calculate wrapping paper for one non cube shaped present 3" do
-    assert wrapping_paper_for_one_present(3, 4, 2) == 32
+    assert wrapping_paper_for_one_present(3, 4, 2) == 58
   end
 
   test "calculate wrapping paper for one non cube shaped present 4" do
-    assert wrapping_paper_for_one_present(3, 2, 4) == 32
+    assert wrapping_paper_for_one_present(3, 2, 4) == 58
   end
 
   test "calculate wrapping paper for one non cube shaped present 5" do
-    assert wrapping_paper_for_one_present(4, 3, 2) == 32
+    assert wrapping_paper_for_one_present(4, 3, 2) == 58
   end
 
   test "calculate wrapping paper for one non cube shaped present 6" do
-    assert wrapping_paper_for_one_present(4, 2, 3) == 32
+    assert wrapping_paper_for_one_present(4, 2, 3) == 58
   end
 
   test "read test input" do
@@ -35,7 +35,15 @@ defmodule Day2_1_Test do
   end
 
   test "create list of input from file" do
-    assert create_list_of_sides("29x13x26\n11x11x14\n27x2x5\n") == [["29", "13", "26"],["11", "11", "14"],["27", "2", "5"]]
+    assert create_list_of_sides("29x13x26\n11x11x14\n27x2x5\n") == [[29, 13, 26],[11, 11, 14],[27, 2, 5]]
+  end
+
+  test "calculate wrapping paper for one present from list input" do
+    assert wrapping_paper_for_one_present([4, 2, 3]) == 58
+  end
+
+  test "calculate wit test file" do
+    assert calculate_with_file("lib/day2/input_for_tests") == 4663
   end
 
 end
