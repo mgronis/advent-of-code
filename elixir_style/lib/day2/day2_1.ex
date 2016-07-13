fb2fae323fe860575647a6c07e7afd306eba27b8
mgronis <m.gronis@gmail.com>
2016-07-13 10:31:45 +0200

    Added first test cases and implementation for day 2

--- elixir_style/lib/day2/day2_1.ex
+++ elixir_style/lib/day2/day2_1.ex
@@ -1,3 +1,54 @@
 defmodule Day2_1 do
 
+  # The elves are running low on wrapping paper, and so they need to submit an
+  # order for more. They have a list of the dimensions (length l, width w, and
+  # height h) of each present, and only want to order exactly as much as they
+  # need.
+
+  # Fortunately, every present is a box (a perfect right rectangular prism),
+  # which makes calculating the required wrapping paper for each gift a little
+  # easier: find the surface area of the box, which is 2*l*w + 2*w*h + 2*h*l.
+  # The elves also need a little extra paper for each present: the area of the
+  # smallest side.
+
+  # For example:
+
+  #    A present with dimensions 2x3x4 requires 2*6 + 2*12 + 2*8 = 52 square
+  #    feet of wrapping paper plus 6 square feet of slack, for a total of 58
+  #    square feet.
+  #    A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square
+  #    feet of wrapping paper plus 1 square foot of slack, for a total of 43
+  #    square feet.
+
+  # All numbers in the elves' list are in feet. How many total square feet of
+  # wrapping paper should they order?
+
+  def wrapping_paper_for_one_present(a, b, c) when a <= b and b <= c do
+    a * b + a * c + c * b + extra_wrapping_paper(a, b)
+  end
+
+  def wrapping_paper_for_one_present(a, b, c) when a <= c and b <= c do
+    a * b + a * c + c * b + extra_wrapping_paper(a, b)
+  end
+
+  def wrapping_paper_for_one_present(a, b, c) when b <= c and c <= a do
+    a * b + a * c + c * b + extra_wrapping_paper(b, c)
+  end
+
+  def wrapping_paper_for_one_present(a, b, c) when a <= c and c <= b do
+    a * b + a * c + c * b + extra_wrapping_paper(a, c)
+  end
+
+  def wrapping_paper_for_one_present(a, b, c) when c <= a and b <= a do
+    a * b + a * c + c * b + extra_wrapping_paper(b, c)
+  end
+
+  def wrapping_paper_for_one_present(a, b, c) when a <= b and c <= a do
+    a * b + a * c + c * b + extra_wrapping_paper(a, c)
+  end
+
+  def extra_wrapping_paper(a, b) do
+    a * b
+  end
+
 end
--- elixir_style/test/day2/day2_1_test.exs
+++ elixir_style/test/day2/day2_1_test.exs
@@ -1,5 +1,33 @@
 defmodule Day2_1_Test do
   use ExUnit.Case
-  import Day2_1 
+  import Day2_1
+
+  test "calculate wrapping paper for one cube shaped present" do
+    assert wrapping_paper_for_one_present(4, 4, 4) == 64
+  end
+
+  test "calculate wrapping paper for one non cube shaped present 1" do
+    assert wrapping_paper_for_one_present(2, 3, 4) == 32
+  end
+
+  test "calculate wrapping paper for one non cube shaped present 2" do
+    assert wrapping_paper_for_one_present(2, 4, 3) == 32
+  end
+
+  test "calculate wrapping paper for one non cube shaped present 3" do
+    assert wrapping_paper_for_one_present(3, 4, 2) == 32
+  end
+
+  test "calculate wrapping paper for one non cube shaped present 4" do
+    assert wrapping_paper_for_one_present(3, 2, 4) == 32
+  end
+
+  test "calculate wrapping paper for one non cube shaped present 5" do
+    assert wrapping_paper_for_one_present(4, 3, 2) == 32
+  end
+
+  test "calculate wrapping paper for one non cube shaped present 6" do
+    assert wrapping_paper_for_one_present(4, 2, 3) == 32
+  end
 
 end
