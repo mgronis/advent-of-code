cd3a7e4e490d0dc47b305d0facfd24c1f9a26cc3
mgronis <m.gronis@gmail.com>
2016-07-14 12:06:12 +0200

    Added more tests and reused some code from previous puzzel from day 2, a bit of refactoring is in order

--- elixir_style/lib/day2/day2_2.ex
+++ elixir_style/lib/day2/day2_2.ex
@@ -21,6 +21,35 @@ defmodule Day2_2 do
 
   # How many total feet of ribbon should they order?
 
+  def calculate do
+    calculate_with_file("lib/day2/input")
+  end
+
+  def calculate_with_file(filename) do
+    read_file(filename)
+    |> create_list_of_sides
+    |> Enum.map(fn(list) -> Enum.sort(list) end)
+    |> Enum.map(fn(sides) -> calculate_ribbon(sides) end)
+    |> Enum.reduce(0, fn (x, acc) -> x + acc end)
+  end
+
+  def read_file(filename) do
+    {:ok, data} = File.open(filename, [:read], fn(file) -> IO.read(file, :all) end)
+    data
+  end
+
+  def create_list_of_sides(string_input) do
+    String.split(string_input, "\n", trim: true)
+    |> Enum.map(
+      fn(str) -> String.split(str, "x")
+      |> Enum.map(fn str -> String.to_integer(str)  end)
+    end)
+  end
+
+  def calculate_ribbon([a, b, c]) do
+    feet_of_ribbon_for_present(a, b, c)
+  end
+
   def feet_of_ribbon_for_present(a, b, c) do
     2*a + 2*b + feet_of_ribbon_for_bow(a, b, c)
   end
--- elixir_style/test/day2/day2_2_test.exs
+++ elixir_style/test/day2/day2_2_test.exs
@@ -10,4 +10,16 @@ defmodule Day2_2_Test do
     assert feet_of_ribbon_for_present(1, 1, 10) == 14
   end
 
+  test "feet of ribbon from list" do
+    assert calculate_ribbon([2, 3, 4]) == 34
+  end
+
+  test "read test input" do
+    assert read_file("test/day2/input_for_tests") == "29x13x26\n11x11x14\n27x2x5\n"
+  end
+
+  test "calculate with test file" do
+    assert calculate_with_file("test/day2/input_for_tests") == 11902
+  end
+
 end
