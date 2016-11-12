38b90606b63e70a081e0c27069f0282e895cffaf
mgronis <m.gronis@gmail.com>
2016-11-12 19:51:10 +0100

    Solved puzzle 2 of day 6

--- elixir_style/lib/day6/day6_2.ex
+++ elixir_style/lib/day6/day6_2.ex
@@ -23,6 +23,77 @@ defmodule Day6_2 do
   #    turn on 0,0 through 0,0 would increase the total brightness by 1.
   #    toggle 0,0 through 999,999 would increase the total brightness by 2000000.
 
+  @initial_grid for n <- 0..999, m <- 0..999, into: %{}, do: {{n, m}, 0}
 
+  def lights do
+    lights "lib/day6/input", get_initial_grid
+  end
+
+  def lights(file_name, grid) do
+    parse_input(file_name)
+    |> flip_lights(grid)
+    |> Map.values()
+    |> Enum.sum()
+  end
+
+  def flip_lights(commands, grid) do
+    flip commands, grid
+  end
+
+  defp flip([{:on, {from_x, from_y}, {to_x, to_y}} | tail], grid) do
+    flip tail, update_grid(grid, from_x, from_y, to_x, to_y, fn(value) -> value + 1 end)
+  end
+
+  defp flip([{:off, {from_x, from_y}, {to_x, to_y}} | tail], grid) do
+    flip tail, update_grid(grid, from_x, from_y, to_x, to_y, fn
+      (0) -> 0
+      (value) -> value - 1
+    end)
+  end
+
+  defp flip([{:toggle, {from_x, from_y}, {to_x, to_y}} | tail], grid) do
+    flip tail, update_grid(grid, from_x, from_y, to_x, to_y, fn(value) -> value + 2 end)
+  end
+
+  defp flip([], grid) do
+    grid
+  end
+
+  defp update_grid(grid, from_x, from_y, to_x, to_y, update_function) do
+    extract_keys(from_x, from_y, to_x, to_y)
+    |> Enum.reduce(grid, fn(key, acc) -> Map.update!(acc, key, update_function) end)
+  end
+
+  defp extract_keys(from_x, from_y, to_x, to_y) do
+    for x <- from_x..to_x, y <- from_y..to_y, do: {x, y}
+  end
+
+  def parse_input(file_name) do
+    File.read!(file_name)
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn(str) -> separator(String.split(str, " ", trim: true)) end)
+  end
+
+  defp separator(["turn", "on", from, _, to | _tail]) do
+    {:on, tuplify(from, ","), tuplify(to, ",")}
+  end
+
+  defp separator(["turn", "off", from, _, to | _tail]) do
+    {:off, tuplify(from, ","), tuplify(to, ",")}
+  end
+
+  defp separator(["toggle", from, _, to | _tail]) do
+    {:toggle, tuplify(from, ","), tuplify(to, ",")}
+  end
+
+  defp tuplify(list, split_char) do
+    String.split(list, split_char, trim: true)
+    |> Enum.map(fn(str) -> String.to_integer(str) end)
+    |> List.to_tuple()
+  end
+
+  def get_initial_grid do
+    @initial_grid
+  end
 
 end
--- elixir_style/test/day6/day6_2_test.exs
+++ elixir_style/test/day6/day6_2_test.exs
@@ -2,8 +2,28 @@ defmodule Day6_2_Test do
   use ExUnit.Case
   import Day6_2
 
-  test "hello" do
-    assert "hello" = "hello"
+  test "flip lights with on command" do
+    assert flip_lights([{:on, {0, 1}, {1, 1}}], test_grid) ==
+      %{ {0,0} => 1, {0,1} => 1, {1,0} => 0, {1,1} => 2}
+  end
+
+  test "flip lights with off command" do
+    assert flip_lights([{:off, {0, 1}, {1, 1}}], test_grid) ==
+      %{ {0,0} => 1, {0,1} => 0, {1,0} => 0, {1,1} => 0}
+  end
+
+  test "flip lights with toggle command" do
+    assert flip_lights([{:toggle, {0, 1}, {1, 1}}], test_grid) ==
+      %{ {0,0} => 1, {0,1} => 2, {1,0} => 0, {1,1} => 3}
+  end
+
+  defp test_grid do
+    %{
+      {0,0} => 1,
+      {0,1} => 0,
+      {1,0} => 0,
+      {1,1} => 1
+    }
   end
 
 end
