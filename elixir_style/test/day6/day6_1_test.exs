74d9b61a93a010f4bea653aa546080b24ca7c9d6
mgronis <m.gronis@gmail.com>
2016-11-12 18:35:15 +0100

    Solved puzzel 1 of day 6

--- elixir_style/test/day6/day6_1_test.exs
+++ elixir_style/test/day6/day6_1_test.exs
@@ -4,8 +4,33 @@ defmodule Day6_1_Test do
 
   test "parse input form test file" do
     assert parse_input("test/day6/input_for_tests") ==
-      [["turn", "on", "489,959", "through", "759,964"],
-      ["turn", "off", "820,516", "through", "871,914"]]
+      [{:on, {0, 1}, {0, 2}},
+      {:off, {1, 1}, {1, 2}},
+      {:toggle, {0, 0}, {2, 1}}]
+  end
+
+  test "flip lights with on command" do
+    assert flip_lights([{:on, {0, 1}, {1, 1}}], test_grid) ==
+      %{ {0,0} => 1, {0,1} => 1, {1,0} => 0, {1,1} => 1}
+  end
+
+  test "flip lights with off command" do
+    assert flip_lights([{:off, {0, 1}, {1, 1}}], test_grid) ==
+      %{ {0,0} => 1, {0,1} => 0, {1,0} => 0, {1,1} => 0}
+  end
+
+  test "flip lights with toggle command" do
+    assert flip_lights([{:toggle, {0, 1}, {1, 1}}], test_grid) ==
+      %{ {0,0} => 1, {0,1} => 1, {1,0} => 0, {1,1} => 0}
+  end
+
+  defp test_grid do
+    %{
+      {0,0} => 1,
+      {0,1} => 0,
+      {1,0} => 0,
+      {1,1} => 1
+    }
   end
 
 end
