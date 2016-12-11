c5e4d05ef0b0893350fb4ba90b8bf18969caff1e
mgronis <m.gronis@gmail.com>
2016-12-11 13:27:01 +0100

    Solved 2015 puzzle 1 of day 7

--- elixir_style/lib/2015/day7/day7_1.ex
+++ elixir_style/lib/2015/day7/day7_1.ex
@@ -1,4 +1,6 @@
+
 defmodule Day7_1 do
+  use Bitwise
 
   # This year, Santa brought little Bobby Tables a set of wires and bitwise
   # logic gates! Unfortunately, little Bobby is a little under the recommended
@@ -58,8 +60,42 @@ defmodule Day7_1 do
     solve "a", "lib/2015/day7/input"
   end
 
-  def solve(lead_name, file_name) do
-    map = parse_input(file_name)
+  def solve(lead, file_name) do
+    instructions = parse_input(file_name)
+    calculate_lead(lead, instructions)[lead]
+  end
+
+  def calculate_lead_for_test(lead, instructions) do
+    calculate_lead(lead, instructions)[lead]
+  end
+
+  defp calculate_lead(lead, instructions) do
+    case instructions[lead] do
+      ["NOT", value] ->
+        instructions = calculate_lead(value, instructions)
+        Map.put(instructions, lead, 65535 ^^^ instructions[value])
+      [value_x, "AND", value_y] ->
+        instructions = calculate_lead(value_x, instructions)
+        instructions = calculate_lead(value_y, instructions)
+        Map.put(instructions, lead, instructions[value_x] &&& instructions[value_y])
+      [value_x, "OR", value_y] ->
+        instructions = calculate_lead(value_x, instructions)
+        instructions = calculate_lead(value_y, instructions)
+        Map.put(instructions, lead, instructions[value_x] ||| instructions[value_y])
+      [value, "LSHIFT", amount] ->
+        instructions = calculate_lead(value, instructions)
+        Map.put(instructions, lead, instructions[value] <<< amount)
+      [value, "RSHIFT", amount] ->
+        instructions = calculate_lead(value, instructions)
+        Map.put(instructions, lead, instructions[value] >>> amount)
+      value when is_binary(value) ->
+        instructions = calculate_lead(value, instructions)
+        Map.put(instructions, lead, instructions[value])
+      value when is_integer(value) ->
+        instructions
+      nil ->
+        Map.put(instructions, lead, lead)
+    end
   end
 
   def parse_input(file_name) do
@@ -68,8 +104,23 @@ defmodule Day7_1 do
     |> Enum.reduce(%{}, fn(str, acc) -> to_map(String.split(str, " -> ", trim: true), acc) end)
   end
 
-  defp to_map([value, key | _tai], acc) do
-    Map.put(acc, key, String.split(value, " ", trim: true))
+  defp to_map([value, key], acc) do
+    Map.put(acc, key, extract_values(value))
+  end
+
+  defp extract_values(value) do
+     case String.split(value, " ", trim: true) do
+       [value] -> as_value(value)
+       [operator, value] -> [operator, as_value(value)]
+       [value_x, operator, value_y] -> [as_value(value_x), operator, as_value(value_y)]
+     end
+  end
+
+  defp as_value(value) do
+    case Integer.parse(value) do
+      :error -> value
+      {number, _} -> number
+    end
   end
 
 end
--- elixir_style/test/2015/day7/day7_1_test.exs
+++ elixir_style/test/2015/day7/day7_1_test.exs
@@ -4,14 +4,58 @@ defmodule Day7_1_Test do
 
   test "pase file" do
     assert parse_input("test/2015/day7/input_for_test") ==
-       %{"x" => ["123"],
-       "y" => ["456"],
+       %{"x" => 123,
+       "y" => 456,
        "d" => ["x", "AND", "y"],
        "e" => ["x", "OR", "y"],
-       "f" => ["x", "LSHIFT", "2"],
-       "g" => ["y", "RSHIFT", "2"],
+       "f" => ["x", "LSHIFT", 2],
+       "g" => ["y", "RSHIFT", 2],
        "h" => ["NOT", "x"],
        "i" => ["NOT", "y"]}
+  end
+
+  test "calculate value" do
+    assert calculate_lead_for_test("f", %{"f" => 1}) == 1
+  end
+
+  test "calculate variable pointing to value" do
+    assert calculate_lead_for_test("f", %{"f" => "a", "a" => 2}) == 2
+  end
+
+  test "calculate NOT" do
+    assert calculate_lead_for_test("f", %{"f" => ["NOT", 1]}) == 65534
+  end
+
+  test "calculate AND with values" do
+    assert calculate_lead_for_test("f", %{"f" => [2, "AND", 1]}) == 0
+  end
+
+  test "calculate AND with variables" do
+    assert calculate_lead_for_test("f", %{"f" => ["d", "AND", "e"], "d" => 3, "e" => 2}) == 2
+  end
+
+  test "calculate OR with values" do
+    assert calculate_lead_for_test("f", %{"f" => [2, "OR", 1]}) == 3
+  end
+
+  test "calculate OR with variables" do
+    assert calculate_lead_for_test("f", %{"f" => ["d", "OR", "e"], "d" => 4, "e" => 2}) == 6
+  end
+
+  test "calculate LSHIFT with values" do
+    assert calculate_lead_for_test("f", %{"f" => [2, "LSHIFT", 1]}) == 4
+  end
+
+  test "calculate LSHIFT with variables" do
+    assert calculate_lead_for_test("f", %{"f" => ["d", "LSHIFT", 2], "d" => 4}) == 16
+  end
+
+  test "calculate RSHIFT with values" do
+    assert calculate_lead_for_test("f", %{"f" => [2, "RSHIFT", 1]}) == 1
+  end
+
+  test "calculate RSHIFT with variables" do
+    assert calculate_lead_for_test("f", %{"f" => ["d", "RSHIFT", 2], "d" => 4}) == 1
   end
 
   test "calculate test data for lead x" do
