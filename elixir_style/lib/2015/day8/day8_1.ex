d8ab804101e990c96b69cb4e9aaa21fa0688141b
mgronis <m.gronis@gmail.com>
2016-12-26 11:56:46 +0100

    Solved 2015 puzzle 1 and 2 of day 8

--- elixir_style/lib/2015/day8/day8_1.ex
+++ elixir_style/lib/2015/day8/day8_1.ex
@@ -37,4 +37,24 @@ defmodule Day8_1 do
   # of string code (2 + 5 + 10 + 6 = 23) minus the total number of characters
   # in memory for string values (0 + 3 + 7 + 1 = 11) is 23 - 11 = 12.
 
+  def solve do
+    solve "lib/2015/day8/input"
+  end
+
+  def solve(file) do
+    File.read!(file)
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(0, fn(x, acc) -> String.length(x) - memory_portion(String.to_char_list(x), -2) + acc end)
+  end
+
+  def memory_portion(list, acc) do
+    case list do
+      [] -> acc
+      [?\\, ?" | t] -> memory_portion(t, acc + 1)
+      [?\\, ?\\ | t] -> memory_portion(t, acc + 1)
+      [?\\, ?x, _, _ | t] -> memory_portion(t, acc + 1)
+      [_ | t] -> memory_portion(t, acc + 1)
+    end
+  end
+
 end
